337fb2783598d9a7c9d12e80de2c98c42580b5c9
Shubh Gupta <shubhorai12@gmail.com>
2024-07-28 00:35:12 +0530

    learnt component in react

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Shubh from './components/Shubh';
+
+function App() {
+  return (
+    <div className='App'>
+      <App/>
+      <Shubh />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-react-app/src/components/Shubh.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Shubh() {
+    return (
+        <h1>Hello world Shubh component</h1>
+    );
+}
+
+export default Shubh;
